42a8babb8b817bca2024dd0a92d8761a4de25d72
Eric Belfer <ericbelf@gmail.com>
2018-07-05 20:42:49 -0400

    installed chai

--- test/test-server.js
+++ test/test-server.js
@@ -18,3 +18,80 @@ describe("index page", function() {
       });
   });
 });
+
+describe("create new routine page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get('/createnewroutine')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
+
+describe("existing workout page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get('/existingworkout')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
+
+describe("login page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get('/login')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
+
+describe("new workout page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get('/newworkout')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
+
+describe("saved workout page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get('/savedworkout')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
+
+describe("select workout page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get('/selectworkout')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
+
+describe("workout tracker page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get('/workouttracker')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
