3d755cb43b44aca9f344bf9b1b3ce125d47cd776
Eric Belfer <ericbelf@gmail.com>
2018-07-07 09:24:23 -0400

    modified test-server

--- test/test-server.js
+++ test/test-server.js
@@ -2,13 +2,21 @@
 
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require("../server.js");
+const {app, runServer, closeServer} = require('../server');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe("index page", function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
   it("should exist", function() {
     return chai
       .request(app)
